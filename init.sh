52908cf727bc252b1012318ab26959dac0927eb3
Roman Savin <romsav@gmail.com>
2016-05-20 14:19:38 +0300

    change all

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo rm -r /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
 sudo /etc/init.d/gunicorn restart
