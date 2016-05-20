432f6b2f60020432a4ead86481f249c301d6b928
Roman Savin <romsav@gmail.com>
2016-05-20 15:19:38 +0300

    change all

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     qs = environ['QUERY_STRING']
-	print qs
+	print(qs)
     ls = qs.split("&")
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [ "\n".join(ls) ]
