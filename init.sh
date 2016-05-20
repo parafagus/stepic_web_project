c6a1110a42f9ba5e088157423990daf329442061
Roman Savin <romsav@gmail.com>
2016-05-20 13:42:35 +0300

    add gunicorn

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
+sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
+sudo rm -r /etc/gunicorn.d/*
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
 sudo /etc/init.d/gunicorn restart
