0a21eae7a04ac13a49dde7188e7e6edab97ff8a3
Roman Savin <romsav@gmail.com>
2016-05-20 14:00:33 +0300

    change all

--- etc/hello.py
+++ etc/hello.py
@@ -11,14 +11,14 @@
 #    ),
 #}
 CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--workers=16',
-        '--timeout=60',
+	'mode': 'wsgi',
+	'working_dir': '/home/box/web',
+	'python': '/usr/bin/python',
+	'args': (
+		'--bind=0.0.0.0:8080',
+		'--workers=16',
+		'--timeout=60',
 		'--log-level=debug',
-        'hello',
-    ),
+		'hello',
+	),
 }
